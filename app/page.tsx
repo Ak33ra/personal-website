ed9f89a2dac59c7019b4a55e264d385e84adb260
Ak33ra <89165104+Ak33ra@users.noreply.github.com>
2024-08-18 17:54:42 -0700

    link revs website

--- app/page.tsx
+++ app/page.tsx
@@ -31,9 +31,9 @@ export default function Home() {
         </a>
 
         <Link
-          href="projects/"
+          href="https://ak33ra.github.io/revs"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target=""
+          target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
